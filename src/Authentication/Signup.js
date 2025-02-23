e002483892e080d27c9d790e4f5bcba0e3bafd10
Yashrxx <yashkumar.s2023@vitstudent.ac.in>
2025-02-23 12:53:23 +0530

    Update Signup backend url
    'https://urbanbasket-backend.onrender.com/api/auth/createuser'

--- src/Authentication/Signup.js
+++ src/Authentication/Signup.js
@@ -19,7 +19,7 @@ const Signup = (props) => {
         }
 
         try {
-            const response = await fetch('http://localhost:5000/api/auth/createuser', {
+            const response = await fetch('https://urbanbasket-backend.onrender.com/api/auth/createuser' , {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
