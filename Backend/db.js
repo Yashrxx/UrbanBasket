729f88a38e3e2521cbf7389abd266c17f5c1c403
Yashrxx <yashkumar.s2023@vitstudent.ac.in>
2025-06-28 17:34:44 +0530

    mongo string changes

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,6 @@
 const mongoose=require("mongoose");
-const mongoURI="mongodb+srv://yashrxx:Kapil_jain01@cluster0.wshex.mongodb.net/urbanBasket?retryWrites=true&w=majority&appName=Cluster0";
+const mongoURI="mongodb+srv://yashrxx:Kapil_jain01@cluster0.wshex.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const connectToMongo=()=>{
-    // console.log("Connected To Mongoose Successfully")
-    // mongoose.connect(mongoURI,()=>{
-    //     console.log("Connected To Mongoose Successfully")
-    // })
     mongoose.connect(mongoURI)
     .then(() => {
         console.log('Connected to MongoDB');
